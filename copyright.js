bdaa98cbe263bfff22e1d92b3024eadf9259e4b7
ilubraw <warbuli@hotmail.com>
2017-05-27 01:21:09 +0200

    update to check that the new updates work...

---
+++ copyright.js
@@ -0,0 +1,5 @@
+document.write('\
+						<ul class="copyright">\
+		<li>&copy; WOODENSPOONSandWHISKS</li><li>Design by <a href="https://html5up.net">LUKE CHIRCOP</a></li>\
+						</ul>\
+');
---
+++ menu.js
@@ -0,0 +1,106 @@
+document.write('\
+<ul class="links">\
+							<strong><dl><h1><a href="index.html">Home</a></h1></dl></strong>\
+					<!-- Menu Seasonal -->\
+							<div class="dropdown">\
+							<strong><dl><h1>Seasonal</h1></dl></strong>\
+								<ul class="seasonalLinks">\
+									<div class="dropdown-content">\
+										<div class="dropdown-submenu">\
+									<dl><h2>New Year<span class="caret"></span></h2></dl>\
+											<div class="dropdown-content">\
+										<h3><font color="lightgrey"><a href="newyear cakes 1.html">Cakes</a></font></h3></div>\
+											<div class="dropdown-content">\
+										<h3><font color="lightgrey"><a href="newyear cupcakes 1.html">Cupcakes</a></font></h3></div>\
+											<div class="dropdown-content">\
+										<h3><font color="lightgrey"><a href="newyear cookies 1.html">Cookies</a></font></h3></div>\
+										<div class="dropdown-submenu">\
+									<dl><h2>Halloween<span class="caret"></span></h2></dl>\
+											<div class="dropdown-content">\
+										<h3><font color="lightgrey"><a href="halloween cakes 1.html">Cakes</a></font></h3></div>\
+											<div class="dropdown-content">\
+										<h3><font color="lightgrey"><a href="halloween cupcakes 1.html">Cupcakes</a></font></h3></div>\
+											<div class="dropdown-content">\
+										<h3><font color="lightgrey"><a href="halloween cookies 1.html">Cookies</a></font></h3></div>\
+										<div class="dropdown-submenu">\
+									<dl><h2>Christmas<span class="caret"></span></h2></dl>\
+											<div class="dropdown-content">\
+										<h3><font color="lightgrey"><a href="christmas cakes 1.html">Cakes</a></font></h3></div>\
+											<div class="dropdown-content">\
+										<h3><font color="lightgrey"><a href="christmas cupcakes 1.html">Cupcakes</a></font></h3></div>\
+											<div class="dropdown-content">\
+										<h3><font color="lightgrey"><a href="christmas cookies 1.html">Cookies</a></font></h3></div>\
+									</div>\
+								</ul>\
+							</div>\
+					<!-- Menu Theme -->\
+							<div class="dropdown">\
+							<strong><dl><h1>Themed</h1></dl></strong>\
+								<ul class="ThemedLinks">\
+									<div class="dropdown-content">\
+										<div class="dropdown-submenu">\
+									<dl><h2>Birthdays and Graduations<span class="caret"></span></h2></dl>\
+												<div class="dropdown-content">\
+											<h3><font color="lightgrey"><a href="birthdays and graduations cakes 1.html">Cakes</a></font></h3></div>\
+												<div class="dropdown-content">\
+											<h3><font color="lightgrey"><a href="birthdays and graduations cupcakes 1.html">Cupcakes</a></font></h3></div>\
+												<div class="dropdown-content">\
+											<h3><font color="lightgrey"><a href="birthdays and graduations cookies 1.html">Cookies</a></font></h3></div>\
+										<div class="dropdown-submenu">\
+									<dl><h2>Weddings and Anniversaries<span class="caret"></span></h2></dl>\
+												<div class="dropdown-content">\
+											<h3><font color="lightgrey"><a href="weddings and anniversaries cakes 1.html">Cakes</a></font></h3></div>\
+												<div class="dropdown-content">\
+											<h3><font color="lightgrey"><a href="weddings and anniversaries cupcakes 1.html">Cupcakes</a></font></h3></div>\
+												<div class="dropdown-content">\
+											<h3><font color="lightgrey"><a href="weddings and anniversaries cookies 1.html">Cookies</a></font></h3></div>\
+										<div class="dropdown-submenu">\
+									<dl><h2>Christening and Baby showers<span class="caret"></span></h2></dl>\
+											<div class="dropdown-content">\
+										<h3><font color="lightgrey"><a href="christening and baby showers cakes 1.html">Cakes</a></font></h3></div>\
+											<div class="dropdown-content">\
+										<h3><font color="lightgrey"><a href="christening and baby showers cupcakes 1.html">Cupcakes</a></font></h3></div>\
+											<div class="dropdown-content">\
+										<h3><font color="lightgrey"><a href="christening and baby showers cookies 1.html">Cookies</a></font></h3></div>\
+									</div>\
+								</ul>\
+							</div>\
+			\		<!-- Menu Macaroons -->	\	\
+							<div class="dropdown">\
+							<strong><dl><h1><a href=" macaroons.html">Macaroons</a></h1></dl></strong>\
+								<ul class="MacaroonsLinks">\
+								</ul>\
+							</div>\
+					<!-- Menu Sweet Pies -->	\	\
+							<div class="dropdown">\
+							<strong><dl><h1><a href="sweet pies.html">Sweet Pies</a></h1></dl></strong>\
+								<ul class="SweetPiesLinks">\
+								</ul>\
+							</div>\
+					<!-- Menu Others -->	\	\
+							<div class="dropdown">\
+							<strong><dl><h1>Others</h1></dl></strong>\
+							<ul class="OthersLinks">\
+									<div class="dropdown-content">\
+										<div class="dropdown-submenu">\
+									<dl><h2>Naughty<span class="caret"></span></h2></dl>\
+											<div class="dropdown-content">\
+										<h3><font color="lightgrey"><a href="naughty cakes 1.html">Cakes</a></font></h3></div>\
+											<div class="dropdown-content">\
+										<h3><font color="lightgrey"><a href="naughty cupcakes 1.html">Cupcakes</a></font></h3></div>\
+											<div class="dropdown-content">\
+										<h3><font color="lightgrey"><a href="naughty cookies 1.html">Cookies</a></font></h3></div>\
+										</div>\
+									</div>\
+							</ul>\
+							</div>\
+					<!-- Menu Contact Us -->	\	\
+							<strong><dl><h1><a href="contactus.html">Contact Us</a></h1></dl></strong>\
+						</ul>\
+					<!-- buttons get started/ log in -->\
+						<ul class="actions vertical">\
+							<li><a href="#" class="button special fit">Get Started</a></li>\
+							<li><a href="#" class="button fit">Log In</a></li>\
+						</ul>\
+\
+');
