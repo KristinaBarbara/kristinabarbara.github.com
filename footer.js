69017ecb4bc28da16dc4a9622b7368a806ac8e10
Luke Chircop <luke.chircop@ricston.com>
2018-09-09 15:53:20 +0200

    Cleaned Footer. - Need to include pintrest link and EMAIL address.
    created cakes/cupcakes/deserts sub folders and included gallery.html file.
    
    Modified Contact US section with cupcake order picture and details that were in footer.

--- footer.js
+++ footer.js
@@ -1,22 +1,13 @@
 document.write(' <section>\
-		<h2>Aliquam sed mauris</h2>\
-		<p>Sed lorem ipsum dolor sit amet et nullam consequat feugiat consequat magna adipiscing tempus etiam dolore veroeros. eget dapibus mauris. Cras aliquet, nisl ut viverra sollicitudin, ligula erat egestas velit, vitae tincidunt odio.</p>\
-		<ul class="actions">\
-			<li><a href="generic.html" class="button">Learn More</a></li>\
-		</ul>\
-	</section>\
-	<section>\
-		<h2>Etiam feugiat</h2>\
+		<h2>Contact US</h2>\
 		<dl class="alt">\
-			<dt>Address</dt>\
-			<dd>1234 Somewhere Road &bull; Nashville, TN 00000 &bull; USA</dd>\
 			<dt>Phone</dt>\
 			<dd>+356 99140392</dd>\
 			<dt>Email</dt>\
-			<dd><a href="#">information@untitled.tld</a></dd>\
+			<dd><a href="#">INSERTEMAIL@HERE.com</a></dd>\
 		</dl>\
 		<ul class="icons">\
-			<li><a href="#" class="icon fa-facebook alt"><span class="label">Facebook</span></a></li>\
+			<li><a href="https://www.facebook.com/woodenspoonsandwhisks/" class="icon fa-facebook alt"><span class="label">Facebook</span></a></li>\
 			<li><a href="#" class="icon fa-pinterest alt"><span class="label">Pinterest</span></a></li>\
 		</ul>\
 	</section>\
